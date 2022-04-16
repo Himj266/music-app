a9948bdc2cdbf4794999542f84703c5215ebc4e9
Himanshu Jain <himanshu.jain@sprinklr.com>
2022-04-16 17:14:32 +0530

    signup routes

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -2,4 +2,54 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookie from "cookie";
 
-export default async () => {};
+import prisma from "lib/prisma";
+
+//types
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const signUp = async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = bcrypt.genSaltSync();
+
+  const { email, password } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (error) {
+    console.log({ error, email, password });
+    res.status(401);
+    res.json({ error: "User already exists" });
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    "hello",
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("HIMJ_ACCESS_TOKEN", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV == "production",
+    })
+  );
+
+  res.json(user);
+};
+
+export default signUp;
