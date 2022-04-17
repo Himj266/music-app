666dedc49fcb44ccda65e4db69c91ca4dced1343
Himanshu Jain <himanshu.jain@sprinklr.com>
2022-04-17 21:08:46 +0530

    signin api route

---
+++ pages/api/signin.ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+
+import prisma from "lib/prisma";
+
+//types
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const signIn = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign(
+        {
+          email: user.email,
+          id: user.id,
+          time: Date.now(),
+        },
+        "hello", // use it in enviornment varisable
+        { expiresIn: "8h" }
+      );
+
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("HIMJ_ACCESS_TOKEN", token, {
+          httpOnly: true,
+          maxAge: 8 * 60 * 60,
+          path: "/",
+          sameSite: "lax",
+          secure: process.env.NODE_ENV == "production",
+        })
+      );
+      res.json(user);
+
+      console.log("himj", { user });
+    } else {
+      res.status(401);
+      res.json({ error: "Email or password incorrect" });
+    }
+  } catch (err) {
+    res.status(401);
+    res.json({ error: "Email or password incorrect" });
+  }
+};
+
+export default signIn;
