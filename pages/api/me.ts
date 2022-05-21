0df8aabfc8635caffedf2e2add4c646bfbccae28
Himanshu Jain <himanshu.jain@sprinklr.com>
2022-05-21 20:00:47 +0530

    add user data

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,5 +1,16 @@
 import { validateRoute } from "lib/authMiddleware";
+import prisma from "lib/prisma";
 
-const me = validateRoute((req, res, user) => res.json(user));
+const me = validateRoute(async (req, res, user) => {
+  const playlistCount = await prisma.playlist.count({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  res.json({ ...user, playlistCount });
+
+  return res;
+});
 
 export default me;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,42 @@
-import type { NextPage } from "next";
-import { GradientLayout } from "components/Layout";
+//libs
+import prisma from "lib/prisma";
 
-const Home: NextPage = () => {
-  return <GradientLayout color="red" />;
+//components
+import { GradientLayout } from "components/Layout";
+import { Artists } from "components/artists";
+
+//hooks
+import { useUserData } from "hooks/useUserData";
+
+//types
+import { Artist } from "types/entities";
+
+interface Props {
+  artists: Artist[];
+}
+
+const Home = ({ artists }: Props): JSX.Element | null => {
+  const { user, loading } = useUserData();
+
+  if (loading) return null;
+
+  return (
+    <GradientLayout
+      color="red"
+      subtitle="profile"
+      roundedImage
+      description={`${user.playlistCount} Playlists`}
+      title={`${user.firstName} ${user.lastName}`}
+    >
+      <Artists artists={artists} />
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return { props: { artists } };
 };
 
 export default Home;
